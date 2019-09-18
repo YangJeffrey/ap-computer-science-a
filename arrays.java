13abfc7361a3915c0980e1081a5ba962bb5131de
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-09-18 11:19:01 -0400

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arrays {
+
+    public static void main(String args[]) {
+    	String[] names = new String [3];
+    	int[] grades = new int [3];
+    	
+    	for (int i = 0; i < 3; i++) {
+    		Scanner obj1 =  new Scanner(System.in);
+    		System.out.println("Name");
+    		String name = obj1.nextLine();	
+    		names[i] = name;
+    		Scanner obj =  new Scanner(System.in);
+    		System.out.println("Grade");
+    		int grade = obj.nextInt();
+    		grades[i] = grade;	
+    	}
+    	
+    	for (int i = 0; i < 3; i++) {
+    		System.out.println(names[i] + " " + grades[i]);
+    	}
+    }
+}
