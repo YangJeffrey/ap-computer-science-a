4b96a1d1ccfeef4130dc5cdb8c9d7c1e64e1882a
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-10-03 12:09:46 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+package mp1;
+
+public class Rectangle {
+	private double length;
+	private double width;
+	
+	public Rectangle(double l, double w) {
+		length = l;
+		setWidth(w);
+	}
+	
+	public Rectangle() {
+		length = 0;
+		setWidth(0);
+	}
+	
+	public double getlength() {
+		return length;
+	}
+	
+	public void setLength(double l) {
+		l = length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double w) {
+		width = w;
+	}
+}
