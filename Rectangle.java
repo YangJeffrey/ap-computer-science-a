6fbacd088aa6a36538f6e5f748626003090166f9
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-10-06 14:21:39 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,43 +1,65 @@
 package mp1;
 
+package com.company;
+
 public class Rectangle {
-	private double length;
-	private double width;
-	
-	public Rectangle(double l, double w) {
-		length = l;
-		setWidth(w);
-	}
-	
-	public Rectangle() {
-		length = 0;
-		setWidth(0);
-	}
-	
-	public double getlength() {
-		return length;
-	}
-	
-	public void setLength(double l) {
-		l = length;
-	}
+    //data members
+    private double length;
+    private double width;
 
-	public double getWidth() {
-		return width;
-	}
+    /* accessor methods for each data member*/
+    public double getlength() {
+        return length;
+    }
 
-	public void setWidth(double w) {
-		width = w;
-	}
-	
-	public String toString() {
-		return("length: " + l + ", width: " + w);
-				
-	}
-	
-	public boolean equal(double l, double w) {
-		if (l )
-				
-	}
-	
+    public double getwidth() {
+        return width;
+    }
+
+    /* modifier methods for each data member */
+    public void setlength(double l) {
+        l = length;
+    }
+
+    public void setwidth(double w) {
+        w = width;
+    }
+
+    /* constructor receives initializes data members */
+    public Rectangle(double l, double w) {
+        length = l;
+        width = w;
+        //setWidth(w);
+    }
+
+    /* overloaded constructor initializes data members to 0 */
+    public Rectangle() {
+        length = 0;
+        width = 0;
+        //setWidth(0);
+    }
+
+    /* Override the toString method to print length and width with labels */
+    public String toString(double l, double w) {
+        return("length: " + l + ", width: " + w);
+    }
+
+    /* Override the equals method determine if objects are equal if length and width are equal */
+    public boolean equals(double l, double w) {
+        if (l == w) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Calculates the area of the rectangle */
+    public static double area(double l, double w) {
+        return (l*w);
+    }
+
+    /* Calculates the perimeter of the rectangle */
+    public static double perimeter(double l, double w) {
+        return (2*l + 2*w);
+    }
 }
