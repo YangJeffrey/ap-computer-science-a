f6b706a26df246c4f8f9643a893067a9d4e0b0a7
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-10-07 12:10:24 -0400

    Create Code.java

---
+++ Code.java
@@ -0,0 +1,14 @@
+public class Code {
+	private String myCode;
+	private String oldCode;
+	
+	public Code (String code) {
+		myCode = code;
+		oldCode = code;
+	}
+	
+	public String getCode() {
+		return myCode;
+	}
+	
+}
