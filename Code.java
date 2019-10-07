24645b73f2a7a4e79af98e09cd711a4e69b752f9
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-10-07 12:13:40 -0400

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,3 +1,5 @@
+package mp1;
+
 public class Code {
 	private String myCode;
 	private String oldCode;
@@ -9,6 +11,13 @@ public class Code {
 	
 	public String getCode() {
 		return myCode;
+	}
+	
+	public void hide (int p1, int p2) {
+		for (int i = p1; i <= p2; i++) {
+			x = myCode.substring(i, i++)
+			myCode.substring(i, i++) = "X";
+		}
 	}
 	
 }
