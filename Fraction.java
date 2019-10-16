82939ce3c3ba44671f4b19e5bc0cae7c4c5c9c84
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-10-16 12:32:44 -0400

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,82 @@
+package mp1;
+
+public class Fraction {
+	private int numerator;
+	private int denominator;
+	
+	//constructor
+	public Fraction(int n, int d) {
+		numerator = n;
+		denominator = d;
+	}
+	
+	//overloaded constructor
+	public Fraction(Fraction f) {
+		numerator = f.getnumerator();
+		denominator = f.getdenominator();
+	}
+	
+	//accessors
+	public int getnumerator() {
+		return numerator;
+	}
+	
+	public int getdenominator() {
+		return denominator;
+	}
+	
+	//modifiers
+	public void setnumerator(int n) {
+		numerator = n;
+	}
+	
+	public void setdenominator(int d) {
+		denominator = d;
+	}
+	
+	//override toString
+	public String toString() {
+		return (numerator + "/" + denominator);
+	}
+	
+	//override equals
+    public boolean equals(Fraction f) {
+        if (f.getnumerator() == numerator && f.getdenominator() == denominator) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+    //add
+    public Fraction add(Fraction f) {
+    	numerator = numerator * f.getdenominator() + f.getnumerator() * denominator;
+    	denominator = denominator * f.getdenominator();
+    	return new Fraction(numerator, denominator);
+    }
+    
+    //sub
+    public Fraction sub(Fraction f) {
+    	numerator = numerator * f.getdenominator() - f.getnumerator() * denominator;
+    	denominator = denominator * f.getdenominator();
+    	return new Fraction(numerator, denominator);
+    }
+    
+    //mult
+    public Fraction mult(Fraction f) {
+    	numerator = numerator * f.getnumerator();
+    	denominator = denominator * f.getdenominator();
+    	return new Fraction(numerator, denominator);
+    }
+    
+    //div
+    public Fraction div(Fraction f) {
+    	numerator = numerator * f.getdenominator();
+    	denominator = denominator * f.getnumerator();
+    	return new Fraction(numerator, denominator);
+    }
+    
+    //reduce
+    
+    
+}
