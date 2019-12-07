9b489ef82ab1ce49c617120e9ef53436f95cf650
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-12-06 19:50:09 -0500

    Update MatrixProgram.java

--- MatrixProgram.java
+++ MatrixProgram.java
@@ -1,4 +1,5 @@
 package mp2;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -6,41 +7,69 @@ import java.util.Scanner;
 //algorithm:
 //print menu
 //prompt user for option
-//read data from file	
-
+//read data from file with first line as dimensions and the rest filling up the matrices in order
+//sum of 2 matrices sums each of the components of the matrices and returns a new matrix
+/difference of 2 matrices substracts each of the components of one of the matrices from another and returns a new matrix
+//scalar multiplication multiplies each component of a matrix by a prompted scalar
+//matrix multiplication finds the product of 2 matrices by using the properties from bilinearity of the product of scalars
 public class MatrixProgram {
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		Scanner matrixfile = new Scanner(new File("Z:\\csa\\MP2\\src\\mp2\\matrix.txt"));
-		int line = matrixfile.nextInt();
-		int[][] matrix = new int[line][line];
-		int[][] matrix2 = new int[line][line];
-		
-		for (int i = 0; i < line * 4; i++) {
-			line = matrixfile.nextInt();
-			int[][] matrix = {{1,2},{3,4}};
-			int[][] matrix2 = {{5,6},{7,8}};
+
+		int dimensions = matrixfile.nextInt();
+		int[][] matrix = new int[dimensions][dimensions];
+		int[][] matrix2 = new int[dimensions][dimensions];
+		Scanner s = new Scanner(System.in);
+		int scalar;
+		boolean running = true;
+		int option;
+
+		matrix = maplinestomatrix(dimensions, matrixfile);
+		matrix2 = maplinestomatrix(dimensions, matrixfile);
+
+		while (running == true) {
+			printMenu();
+			option = menuOption();
+			if (option == 1) {
+				printMatrix(matrix);
+			} else if (option == 2) {
+				printMatrix(addMatrix(matrix, matrix2));
+			} else if (option == 3) {
+				scalar = s.nextInt();
+				printMatrix(scalarMultiplication(matrix, scalar));
+			} else if (option == 4) {
+				printMatrix(multiplyMatrix(matrix, matrix2));
+			} else if (option == 5) {
+				printMatrix(multiplyMatrix(matrix2, matrix));
+			} else if (option == 6) {
+				printMatrix(subtractMatrix(matrix, matrix2));
+			} else if (option == 7) {
+				printMatrix(subtractMatrix(matrix2, matrix));
+			} else if (option == 8) {
+				running = false;
+			}
 		}
-		
-		int[][] matrix = {{1,2},{3,4}};
-		int[][] matrix2 = {{5,6},{7,8}};
-		
-		printMenu();
-		
-		if (menuOption() == 1) {
-			addMatrix(matrix, matrix2);
-		}
-		
-		
+
 	}
-	
+
+	public static int[][] maplinestomatrix(int dimensions, Scanner matrixfile) {
+		int[][] newmatrix = new int[dimensions][dimensions];
+		for (int i = 0; i < dimensions; i++) {
+			for (int j = 0; j < dimensions; j++) {
+				newmatrix[i][j] = matrixfile.nextInt();
+			}
+		}
+		return newmatrix;
+	}
+
 	/* Get the menu option selected by user */
 	public static int menuOption() {
-		Scanner obj =  new Scanner(System.in);
+		Scanner obj = new Scanner(System.in);
 		int option = obj.nextInt();
 		return option;
 	}
-	
+
 	/* Print the menu */
 	public static void printMenu() {
 		System.out.println("Matrix Program");
@@ -53,7 +82,7 @@ public class MatrixProgram {
 		System.out.println("7. Differences of two matrices (B - A)");
 		System.out.println("8. Exit");
 	}
-	
+
 	/* Print out a matrix in row column form */
 	public static void printMatrix(int matrix[][]) {
 		for (int row = 0; row < matrix.length; row++) {
@@ -63,7 +92,7 @@ public class MatrixProgram {
 			System.out.println();
 		}
 	}
-	
+
 	/* Add one matrix to another */
 	public static int[][] addMatrix(int[][] matrix, int[][] matrix2) {
 		int[][] newmatrix = new int[matrix.length][matrix.length];
@@ -74,7 +103,7 @@ public class MatrixProgram {
 		}
 		return newmatrix;
 	}
-	
+
 	/* Subtracts one matrix from another */
 	public static int[][] subtractMatrix(int[][] matrix, int[][] matrix2) {
 		int[][] newmatrix = new int[matrix.length][matrix.length];
@@ -85,8 +114,8 @@ public class MatrixProgram {
 		}
 		return newmatrix;
 	}
-	
-	/* Multiply a scalar with a matrix*/
+
+	/* Multiply a scalar with a matrix */
 	public static int[][] scalarMultiplication(int[][] matrix, int scalar) {
 		int[][] newmatrix = new int[matrix.length][matrix.length];
 		for (int row = 0; row < matrix.length; row++) {
@@ -96,22 +125,22 @@ public class MatrixProgram {
 		}
 		return newmatrix;
 	}
-	
+
 	/* Multiplies two matrices together */
 	public static int[][] multiplyMatrix(int[][] matrix, int[][] matrix2) {
 		int[][] newmatrix = new int[matrix.length][matrix.length];
 		for (int row = 0; row < matrix.length; row++) {
 			for (int col = 0; col < matrix[0].length; col++) {
 				int sum = 0;
-					for (int k = 0; k < matrix.length; k++) {
+				for (int k = 0; k < matrix.length; k++) {
 					sum += matrix[row][k] * matrix2[k][col];
 				}
 				newmatrix[row][col] = sum;
 			}
 		}
 		return newmatrix;
-	}	
-	
+	}
+
 	private static int singleMatrixDotProduct(int[][] matrix, int[][] matrix2, int i, int j) {
 		int sum = 0;
 		for (int k = 0; k < matrix.length; k++) {
@@ -120,6 +149,5 @@ public class MatrixProgram {
 		return sum;
 	}
 }
-
 
 
