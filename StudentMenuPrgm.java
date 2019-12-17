a105a30a1e565f8c7a53ebca14df39837be53895
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-12-16 21:32:54 -0500

    Update StudentMenuPrgm.java

--- StudentMenuPrgm.java
+++ StudentMenuPrgm.java
@@ -2,39 +2,40 @@ package mp1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class StudentMenuProgram {
 	public static void main(String[] args) throws FileNotFoundException {
 		List<Student> students = new ArrayList<Student>();
-		Student s = new Student("Jeffrey", 2393, 10, 4.0);
-		students.add(s);
-		
+		Student s1 = new Student("Jeffrey", 2393, 10, 4.0);
+		students.add(s1);
+
 		Scanner studentfile = new Scanner(new File("C:\\Users\\yangj\\eclipse-workspace\\MP1\\mp1\\mp1\\studentfile"));
 		String name = studentfile.next();
 		int id = studentfile.nextInt();
 		int grade = studentfile.nextInt();
 		double gpa = studentfile.nextDouble();
-		
+
 		Student s2 = new Student("Jeffrey", 2393, 10, 4.0);
 		students.add(s2);
-		
-		toString();
-		
-		//System.out.println(name + " " + id + " " + grade + " " + gpa);
-		
+
+		//s2.toString(s2);
+
+		// System.out.println(name + " " + id + " " + grade + " " + gpa);
+
+		Iterator<Student> i = students.iterator();
+
+		while (i.hasNext()) {
+			System.out.print(i.next() + " ");
+		}
+
 	}
-	
-	public static void printMenu() {
+
+	public void printMenu() {
 		System.out.println("Student Menu");
-		System.out.println("1. Add");
-		System.out.println("2. Delete");
-		System.out.println("3. Change");
 	}
-	
-	public static int menuOption() {
+
+	public int menuOption() {
 		Scanner obj = new Scanner(System.in);
 		int option = obj.nextInt();
 		return option;
