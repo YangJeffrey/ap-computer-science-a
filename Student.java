787c937b5474ce9a23d32b0327dede40d5992a7b
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-12-23 13:38:05 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -45,14 +45,14 @@ public class Student {
         }
     }
     
-    public void toString(Student s) {
-    	System.out.println("Name: " + s.getname());
-    	System.out.println("ID: " + s.getid());
-    	System.out.println("Grade: " + s.getgrade());
-    	System.out.println("GPA: " + s.getgpa());
-    }
+//    public void toString(Student s) {
+//    	System.out.println("Name: " + s.getname());
+//    	System.out.println("ID: " + s.getid());
+//    	System.out.println("Grade: " + s.getgrade());
+//    	System.out.println("GPA: " + s.getgpa());
+//    }
     
-//	public String toString(Student s) {
-//		return ("Name: " + s.getname() + "\n" + "ID: " + s.getid() + "\n" + "Grade: " + s.getgrade() + "\n" + "GPA: " + s.getgpa());
-//	}
+	public String toString(Student s) {
+		return ("Name: " + s.getname() + " ID: " + s.getid() + " Grade: " + s.getgrade() + " GPA: " + s.getgpa());
+	}
 }
